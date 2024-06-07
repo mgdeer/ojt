5be2b60f6bf63b00d9efd9e48611f08e539ebd64
yunseok kim <skyblue0728@daum.net>
2024-06-07 18:31:57 +0900

    아이디 자동생성

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,13 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Member;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
+// public interface MemberRepository extends JpaRepository<Member, String> {
+// }
 public interface MemberRepository extends JpaRepository<Member, Long> {
-   boolean existsByEmail(String email);
 
-    Optional<Member> findByEmail(String email);
+    @Query("select concat(substring(curdate(),1,4),lpad(count(*)+1, '5','0') ) from Member m where year(m.createdDate) = year(curdate())")
+    String makeId();
 }
