697f9361ff13d3ed3c3d466b7cf320a1f110a438
jeonminhun <a01066215336@gmail.com>
2024-06-10 10:28:18 +0900

    MemberRepository make_Id

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Member;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -10,4 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
    boolean existsByEmail(String email);
 
     Optional<Member> findByEmail(String email);
+
+    @Query("select concat(substring(curdate(),1,4),lpad(count(*)+1, '5','0') ) from Member m where year(m.createdDate) = year(curdate())")
+    String makeId();
 }
