c9cb66d1bdea83db362355eb3f1c89ca31d7ad03
HOSIHO <compleat777@gmail.com>
2024-06-11 13:35:11 +0900

    RUD

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import com.example.demo.dto.MemberRequestDto;
 import com.example.demo.dto.MemberResponseDto;
 import com.example.demo.dto.TokenRequestDto;
+import com.example.demo.entity.Member;
 import com.example.demo.jwt.TokenDto;
 import com.example.demo.service.MemberService;
 import com.example.demo.dto.UserCreateForm;
@@ -45,4 +48,42 @@ public class MemberController {
     public ResponseEntity<Boolean> phoneCheck(@PathVariable(name = "phone") String phone) {
         return memberService.phoneCheck(phone);
     }
+
+    @GetMapping
+    public List<Member> listMembers() {
+        return memberService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Member getMember(@PathVariable Long id) {
+        return memberService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Member editMember(@PathVariable Long id, @RequestBody Member member) {
+        member.setId(id);
+        memberService.update(member);
+        return member;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMember(@PathVariable Long id) {
+        memberService.delete(id);
+    }
+
+    @GetMapping("/me")
+    public Member myInfo() {
+
+        Long loggedInUserId = 1L; // 로그인한 아이디 세션 받아오기
+        return memberService.findById(loggedInUserId);
+    }
+
+    @PutMapping("/me")
+    public Member updateMyInfo(@RequestBody Member member) {
+        memberService.updateMyInfo(member);
+        return member;
+    }
+
+
+
 }
